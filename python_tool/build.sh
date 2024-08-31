63c4282e8147a32f0f6dde89683f69b95f28a979
githubtohaoyangli <fairzlq@hotmail.com>
2024-08-31 12:19:59 +0800

    update

--- python_tool/build.sh
+++ python_tool/build.sh
@@ -1,5 +1,5 @@
 
 
 
-python -m nuitka --standalone --macos-create-app-bundle --remove-output --enable-plugins=tk-inter --macos-app-name=Python_tool --macos-app-version=1.1.0 --show-progress --output-dir=dist --macos-app-icon=python_tool.icns python_tool.py
+python -m nuitka --standalone --macos-create-app-bundle --remove-output --enable-plugins=tk-inter --macos-app-name=Python_tool --macos-app-version=1.1.0 --show-progress --macos-app-icon=python_tool.icns python_tool.py
 
