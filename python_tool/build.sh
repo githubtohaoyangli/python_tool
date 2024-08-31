5497ad0cb646eb3d89d240e7733bd5607a4f81f5
githubtohaoyangli <fairzlq@hotmail.com>
2024-08-31 17:30:26 +0800

    python-tool

--- python_tool/build.sh
+++ python_tool/build.sh
@@ -1,5 +1,5 @@
 
 
-pyinstaller -F -w -i python_tool.icns --name "python_tool" ./python_tool/python_tool.py
+pyinstaller -F -w -i python_tool.icns --name "python_tool" python_tool.py
 #python -m nuitka --standalone --macos-create-app-bundle --remove-output --enable-plugins=tk-inter --macos-app-name=Python_tool --macos-app-version=1.1.0 --show-progress --macos-app-icon=python_tool.icns python_tool.py
 
