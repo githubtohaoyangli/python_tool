09d64a65937b3c4443d9b24aafb710303338e4ed
githubtohaoyangli <fairzlq@hotmail.com>
2024-10-07 18:17:09 +0800

    整合

--- updater/build.sh
+++
@@ -1,5 +0,0 @@
-
-
-
-python -m nuitka --clang --remove-output --show-progress --standalone --output-dir=udist --macos-create-app-bundle --macos-app-name=Updater --enable-plugins=tk-inter --macos-app-version=1.0.0 --macos-app-icon=python_tool.icns update.py
-
