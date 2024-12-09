7105b148395c4c144c1835f5ae56001a941e8baf
githubtohaoyangli <138438187+githubtohaoyangli@users.noreply.github.com>
2024-12-09 13:37:02 +0800

    Update p.py

--- pa/p.py
+++ pa/p.py
@@ -1,7 +1,8 @@
 import sys
-from qfluentwidgets import PushButton, VBoxLayout,PillPushButton,PrimaryPushButton,TogglePushButton,CalendarPicker, FluentTranslator
-from PyQt5.QtWidgets import  QWidget,QMainWindow,QApplication,QVBoxLayout
+from qfluentwidgets import PushButton, VBoxLayout,PillPushButton,PrimaryPushButton,TogglePushButton,CalendarPicker, FluentTranslator,DatePicker,FastCalendarPicker,label
+from PyQt5.QtWidgets import  QWidget,QMainWindow,QApplication,QVBoxLayout,QLabel
 from enum import Enum
+from qfluentwidgets import FluentIcon as FIF
 class MainWindow(QMainWindow):
 
     def __init__(self):
@@ -16,19 +17,21 @@ class MainWindow(QMainWindow):
         #self.setGeometry(100, 100, 300, 200)
 
         # 创建一个按钮
-        self.buttona = PushButton("hello", self)
+        self.buttona = PushButton("hello", self,FIF.ACCEPT)
         # 将按钮的点击事件连接到 on_click 槽函数
         self.buttona.clicked.connect(self.on_click)
         self.buttonb=TogglePushButton("you",self)
         self.buttonb.clicked.connect(self.on_click)
-        self.cale=CalendarPicker(self)
+        self.cale=FastCalendarPicker(self)
+        self.cale.dateChanged.connect(self.cal)
+        self.laba=QLabel("youe",self)
         # 创建一个布局管理器
         layout = QVBoxLayout()
         # 将按钮添加到布局管理器中
         layout.addWidget(self.buttona)
         layout.addWidget(self.buttonb)
         layout.addWidget(self.cale)
-
+        layout.addWidget(self.laba)
         # 创建一个中心部件并将布局添加到其中
         container = QWidget()
         container.setLayout(layout)
@@ -42,6 +45,9 @@ class MainWindow(QMainWindow):
         当按钮被点击时，打印一条消息到控制台
         """
         print("Button clicked!")
+    def cal(self):
+        print(str(str(str(self.cale.date).split(".")[-1]).split("(")[-1]).strip(")"))
+        
 
 
 if __name__ == "__main__":
